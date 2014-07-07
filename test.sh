dd155df525c47d795fb49c37ae8f2befd05f12cc
JimJarvis <jimfanspire@gmail.com>
2014-07-07 01:21:49 -0400

    Now test.sh script is able to perform JUnit testing on cmd line. ./test.sh 'c' recompiles native libs, while ./test.sh <blabla> does the unit testing

--- test.sh
+++ test.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
+# to recompile the native libraries, ./test.sh c
+# To do JUnit testing, ./test.sh blabla  (whatever string not 'c')
 
 main="gpu/SoftmaxTest"
 main="gpu/MinibatchTest"
-main="MiscTest"
 main="gpu/RandTest"
-main="deep/SimpleSigmoidTest"
+main="MiscTest"
+
+units="test.deep.SigmoidNetTest"
+
 
 cp MyThrust/*.h bin/gpu/
 cp matlab/*.txt bin/
@@ -12,7 +16,7 @@ cp matlab/*.txt bin/
 cd bin
 
 # an arbitrary command line arg tells the script to regenerate
-if [ ! -z "$1" ]; then
+if [[ $# -ne 0 && $1 == "c" ]]; then
 java -jar "E:/Dropbox/Programming/Java/JarvisJava/BabelGPU/native/javacpp.jar" gpu/ThrustNative gpu/Thrust -properties windows-x86_64-cuda #> stderr.log
     
     rm -rf ../native/windows-x86_64
@@ -23,4 +27,8 @@ fi
 
 echo ;echo
 echo 'Testing ...'; echo
+if [[ $# -eq 0 ]]; then
 java -Djava.library.path="E:/Dropbox/Programming/Java/Libraries/JCuda" -cp ".;E:/Dropbox/Programming/Java/Libraries/JCuda/jcuda-0.6.0.jar;E:/Dropbox/Programming/Java/Libraries/JCuda/jcublas-0.6.0.jar;E:/Dropbox/Programming/Java/Libraries/JCuda/jcurand-0.6.0.jar;E:/Dropbox/Programming/Java/Libraries/JavaCpp/javacpp.jar" test/$main
+else
+java -Djava.library.path="E:/Dropbox/Programming/Java/Libraries/JCuda" -cp ".;E:/Dropbox/Programming/Java/Libraries/JCuda/jcuda-0.6.0.jar;E:/Dropbox/Programming/Java/Libraries/JCuda/jcublas-0.6.0.jar;E:/Dropbox/Programming/Java/Libraries/JCuda/jcurand-0.6.0.jar;E:/Dropbox/Programming/Java/Libraries/JavaCpp/javacpp.jar;E:/Dropbox/Programming/Java/Libraries/JUnit/junit-4.jar;E:/Dropbox/Programming/Java/Libraries/JUnit/hamcrest.jar" org.junit.runner.JUnitCore $units
+fi
